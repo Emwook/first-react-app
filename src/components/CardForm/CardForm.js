e025ed623bd39b25e4d4ac3723a13da217cbbfcf
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-02-23 17:29:16 +0100

    adding cards

--- src/components/CardForm/CardForm.js
+++ src/components/CardForm/CardForm.js
@@ -1,25 +1,23 @@
-import styles from './ColumnForm.module.scss';
+import styles from './CardForm.module.scss';
 import { useState } from 'react';
-import Button from '../Button/Button.js';
 import TextInput from '../TextInput/TextInput.js';
+import Button from '../Button/Button.js';
 
-const ColumnForm = props => {
+const CardForm = props => {
+
     const [title, setTitle] = useState('');
-    const [icon, setIcon] = useState('');
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: title, icon: icon });
-        setIcon('');
+        props.addCard({ title: title }, props.columnId);
         setTitle('');
     };
 
-	return (
+    return (
         <form className={styles.columnForm} onSubmit={handleSubmit}>
             <span>Title: </span><TextInput value={title} onChange={e => setTitle(e.target.value)} />
-            <span>Icon: </span><TextInput value={icon} onChange={e => setIcon(e.target.value)} />
             <Button>Add column</Button>
         </form>
-	);
+    );
 };
 
-export default ColumnForm;
+export default CardForm;
