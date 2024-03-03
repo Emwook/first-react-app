8603f0de550e71f840f266796dc9c52e969c9dda
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-03-03 13:04:07 +0100

    enabling searchform to filter cards

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -4,9 +4,9 @@ import CardForm from '../CardForm/CardForm.js';
 import { useSelector } from 'react-redux';
 
 const Column = (props) => {
-
-    const cards = useSelector(state => state.cards.filter(card => card.columnId ===props.id))
-
+    const searchKey = useSelector(state => state.search.searchKey);
+    const cards = useSelector(state => state.cards.filter(card => card.columnId ===props.id && card.title.toLowerCase().includes(searchKey.toLowerCase())));
+    
     return (
         <article className={styles.column}>
             <h2 className={styles.title}>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,12 +1,24 @@
 import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 const SearchForm = () => {
+    const dispatch = useDispatch();
+    const searchKey = useSelector(state => state.searchKey);
+    const [localKey, setLocalKey] = useState(searchKey);
+
+    const findKey = e =>{
+        e.preventDefault();
+        dispatch({ type: 'CHANGE_SEARCH_KEY', payload: { searchKey: localKey } });
+        setLocalKey('');
+    };
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder='Search…' />
+        <form className={styles.searchForm} onSubmit={e => {findKey(e)}}>
+            <TextInput value={localKey} onChange={e => {setLocalKey(e.target.value);}} placeholder={'search...'}/>
             <Button>
                 <span className="fa fa-search" />
             </Button>
