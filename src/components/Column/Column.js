2d96e293902a07cb0e782916e56390c6fcbb1284
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-03-03 10:58:56 +0100

    changing List.js and Column.js to use data from redux store

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,8 +1,12 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card.js';
 import CardForm from '../CardForm/CardForm.js';
+import { useSelector } from 'react-redux';
 
 const Column = (props) => {
+
+    const cards = useSelector(state => state.cards.filter(card => card.columnId ===props.id))
+
     return (
         <article className={styles.column}>
             <h2 className={styles.title}>
@@ -11,7 +15,7 @@ const Column = (props) => {
             </h2>
             
             <ul className={styles.cards}>
-                {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+                {cards.map(card => <Card key={card.id} title={card.title} />)}
             </ul>
             <CardForm columnId={props.id} addCard={props.addCard} />
         </article>
