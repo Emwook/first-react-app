f3473a7b6fa2f810f5ac0ab968e14b35f390ff4d
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-03-27 18:02:44 +0100

    fixed routing, added 404 site

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home.js';
 import About from './components/About/About.js';
 import Favorite from './components/Favorite/Favorite.js';
 import List from './components/List/List.js';
+import NotFound from './components/NotFound/NotFound.js';
 import { Routes, Route } from 'react-router-dom';
 
 
@@ -13,10 +14,11 @@ const App = () => {
       <NavBar/>
       <Container>
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/about" element={<About/>}/>
-          <Route path="/favorite" element={<Favorite/>}/>
-          <Route path="/list/:listId" element={<List/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/favorite" element={<Favorite/>}/>
+            <Route path="/list/:listId" element={<List/>}/>
+            <Route path="/*" element={<NotFound/>}/>
         </Routes>
       </Container>
     </main>
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import styles from "./NotFound.module.scss";
+const NotFound = () => {
+  
+    return (
+      <div>
+          <h2 className={styles.title}> 404 - Site not found, sorry ;/</h2>
+      </div>
+    );
+  };
+  
+  export default NotFound;
+  
